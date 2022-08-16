6487d0dfc53020b2b7262b4ee128205aa2818e68
OEM Configuration (temporary user) <rodolfoslv@gmail.com>
2022-08-16 14:29:01 +0200

    create checkout page, checkout items, checkout total

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,9 +5,16 @@ import CartItem from "../cart-item/cart-item.component";
 import { CartContext } from "../../contexts/cart.context";
 
 import "./cart-dropdown.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+      navigate('/checkout');
+  }
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -16,7 +23,7 @@ const CartDropdown = () => {
         ))}
       </div>
 
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={ goToCheckoutHandler }>GO TO CHECKOUT</Button>
     </div>
   );
 };
